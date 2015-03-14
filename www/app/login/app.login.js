052e19ae8f69658838be02c4591e78e49bc8ce48
PBogdan <piotr.bogdan@evercode.com.pl>
2015-03-14 04:02:09 +0100

    przykład wywołania powiadomienia na ekranie logowania

--- www/app/login/app.login.js
+++ www/app/login/app.login.js
@@ -1,2 +1,12 @@
 angular.module('BTW.login', [])
-	.controller('LoginCtrl', function ($scope) {});
+	.controller('LoginCtrl', function ($scope) {
+		$scope.notify = function () {
+			cordova.plugins.notification.local.schedule({
+				id: 1,
+				text: "Beacon in range",
+				at: 'now',
+				icon: 'file://img/clockwht.png',
+				smallIcon: 'file://img/clockblk.png'
+			});
+		}
+	});
